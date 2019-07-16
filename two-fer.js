fa8a21c0e18631b5f96b2e2b199ff4ac287d950e
Juliane Nagao <julianens@gmail.com>
2019-07-16 19:09:39 +0200

    refactor: twoFer function

--- two-fer.js
+++ two-fer.js
@@ -1,4 +1,1 @@
-export const twoFer = name => {
-  if (!name) return 'One for you, one for me.';
-  return `One for ${name}, one for me.`
-};
+export const twoFer = (name = 'you') => `One for ${name}, one for me.`;
